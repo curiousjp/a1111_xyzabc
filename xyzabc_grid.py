209b2999e78f8bb391787e9b5ba536142f9be7a7
curious.jp <48515264+curiousjp@users.noreply.github.com>
2024-01-11 10:00:16 +1100

    Fixes for step count, removal of unused code

--- xyzabc_grid.py
+++ xyzabc_grid.py
@@ -6,7 +6,6 @@ import random
 import csv
 import os.path
 from io import StringIO
-from PIL import Image
 import numpy as np
 
 import modules.scripts as scripts
@@ -27,39 +26,29 @@ fill_values_symbol = "\U0001f4d2"  # 📒
 
 AxisInfo = namedtuple('AxisInfo', ['axis', 'values'])
 
-
 def apply_field(field):
     def fun(p, x, xs):
         setattr(p, field, x)
-
     return fun
-
 
 def apply_prompt(p, x, xs):
     if xs[0] not in p.prompt and xs[0] not in p.negative_prompt:
         raise RuntimeError(f"Prompt S/R did not find {xs[0]} in prompt or negative prompt.")
-
     p.prompt = p.prompt.replace(xs[0], x)
     p.negative_prompt = p.negative_prompt.replace(xs[0], x)
 
-
 def apply_order(p, x, xs):
     token_order = []
-
     # Initally grab the tokens from the prompt, so they can be replaced in order of earliest seen
     for token in x:
         token_order.append((p.prompt.find(token), token))
-
     token_order.sort(key=lambda t: t[0])
-
     prompt_parts = []
-
     # Split the prompt up, taking out the tokens
     for _, token in token_order:
         n = p.prompt.find(token)
         prompt_parts.append(p.prompt[0:n])
         p.prompt = p.prompt[n + len(token):]
-
     # Rebuild the prompt with the tokens in the order we want
     prompt_tmp = ""
     for idx, part in enumerate(prompt_parts):
@@ -67,12 +56,10 @@ def apply_order(p, x, xs):
         prompt_tmp += x[idx]
     p.prompt = prompt_tmp + p.prompt
 
-
 def confirm_samplers(p, xs):
     for x in xs:
         if x.lower() not in sd_samplers.samplers_map:
             raise RuntimeError(f"Unknown sampler: {x}")
-
 
 def apply_checkpoint(p, x, xs):
     info = modules.sd_models.get_closet_checkpoint_match(x)
@@ -80,32 +67,26 @@ def apply_checkpoint(p, x, xs):
         raise RuntimeError(f"Unknown checkpoint: {x}")
     p.override_settings['sd_model_checkpoint'] = info.name
 
-
 def confirm_checkpoints(p, xs):
     for x in xs:
         if modules.sd_models.get_closet_checkpoint_match(x) is None:
             raise RuntimeError(f"Unknown checkpoint: {x}")
 
-
 def confirm_checkpoints_or_none(p, xs):
     for x in xs:
         if x in (None, "", "None", "none"):
             continue
-
         if modules.sd_models.get_closet_checkpoint_match(x) is None:
             raise RuntimeError(f"Unknown checkpoint: {x}")
 
-
 def apply_clip_skip(p, x, xs):
     opts.data["CLIP_stop_at_last_layers"] = x
-
 
 def apply_upscale_latent_space(p, x, xs):
     if x.lower().strip() != '0':
         opts.data["use_scale_latent_for_hires_fix"] = True
     else:
         opts.data["use_scale_latent_for_hires_fix"] = False
-
 
 def find_vae(name: str):
     if name.lower() in ['auto', 'automatic']:
@@ -120,18 +101,14 @@ def find_vae(name: str):
         else:
             return modules.sd_vae.vae_dict[choices[0]]
 
-
 def apply_vae(p, x, xs):
     modules.sd_vae.reload_vae_weights(shared.sd_model, vae_file=find_vae(x))
-
 
 def apply_styles(p: StableDiffusionProcessingTxt2Img, x: str, _):
     p.styles.extend(x.split(','))
 
-
 def apply_uni_pc_order(p, x, xs):
     opts.data["uni_pc_order"] = min(x, p.steps - 1)
-
 
 def apply_face_restore(p, opt, x):
     opt = opt.lower()
@@ -143,9 +120,7 @@ def apply_face_restore(p, opt, x):
         p.face_restoration_model = 'GFPGAN'
     else:
         is_active = opt in ('true', 'yes', 'y', '1')
-
     p.restore_faces = is_active
-
 
 def apply_override(field, boolean: bool = False):
     def fun(p, x, xs):
@@ -154,56 +129,44 @@ def apply_override(field, boolean: bool = False):
         p.override_settings[field] = x
     return fun
 
-
 def boolean_choice(reverse: bool = False):
     def choice():
         return ["False", "True"] if reverse else ["True", "False"]
     return choice
 
-
 def format_value_add_label(p, opt, x):
     if type(x) == float:
         x = round(x, 8)
-
     return f"{opt.label}: {x}"
-
 
 def format_value(p, opt, x):
     if type(x) == float:
         x = round(x, 8)
     return x
 
-
 def format_value_join_list(p, opt, x):
     return ", ".join(x)
-
 
 def do_nothing(p, x, xs):
     pass
 
-
 def format_nothing(p, opt, x):
     return ""
-
 
 def format_remove_path(p, opt, x):
     return os.path.basename(x)
 
-
 def str_permutations(x):
     """dummy function for specifying it in AxisOption's type when you want to get a list of permutations"""
     return x
-
 
 def list_to_csv_string(data_list):
     with StringIO() as o:
         csv.writer(o).writerow(data_list)
         return o.getvalue().strip()
 
-
 def csv_string_to_list_strip(data_str):
     return list(map(str.strip, chain.from_iterable(csv.reader(StringIO(data_str)))))
-
 
 class AxisOption:
     def __init__(self, label, type, apply, format_value=format_value_add_label, confirm=None, cost=0.0, choices=None, prepare=None):
@@ -273,114 +236,6 @@ axis_options = [
     AxisOption("RNG source", str, apply_override("randn_source"), choices=lambda: ["GPU", "CPU", "NV"]),
 ]
 
-
-def draw_xyza_grid(p, processed_x, processed_y, processed_z, processed_a, cell, axes_order):
-    list_size = (len(processed_x) * len(processed_y) * len(processed_z) * len(processed_a))
-
-    processed_result = None
-
-    state.job_count = list_size * p.n_iter
-
-    def process_cell(x, y, z, ix, iy, iz):
-        nonlocal processed_result
-
-        def index(ix, iy, iz):
-            return ix + iy * len(xs) + iz * len(xs) * len(ys)
-
-        state.job = f"{index(ix, iy, iz) + 1} out of {list_size}"
-
-        processed: Processed = cell(x, y, z, ix, iy, iz)
-
-        if processed_result is None:
-            # Use our first processed result object as a template container to hold our full results
-            processed_result = copy(processed)
-            processed_result.images = [None] * list_size
-            processed_result.all_prompts = [None] * list_size
-            processed_result.all_seeds = [None] * list_size
-            processed_result.infotexts = [None] * list_size
-            processed_result.index_of_first_image = 1
-
-        idx = index(ix, iy, iz)
-        if processed.images:
-            # Non-empty list indicates some degree of success.
-            processed_result.images[idx] = processed.images[0]
-            processed_result.all_prompts[idx] = processed.prompt
-            processed_result.all_seeds[idx] = processed.seed
-            processed_result.infotexts[idx] = processed.infotexts[0]
-        else:
-            cell_mode = "P"
-            cell_size = (processed_result.width, processed_result.height)
-            if processed_result.images[0] is not None:
-                cell_mode = processed_result.images[0].mode
-                # This corrects size in case of batches:
-                cell_size = processed_result.images[0].size
-            processed_result.images[idx] = Image.new(cell_mode, cell_size)
-
-    if first_axes_processed == 'x':
-        for ix, x in enumerate(xs):
-            if second_axes_processed == 'y':
-                for iy, y in enumerate(ys):
-                    for iz, z in enumerate(zs):
-                        process_cell(x, y, z, ix, iy, iz)
-            else:
-                for iz, z in enumerate(zs):
-                    for iy, y in enumerate(ys):
-                        process_cell(x, y, z, ix, iy, iz)
-    elif first_axes_processed == 'y':
-        for iy, y in enumerate(ys):
-            if second_axes_processed == 'x':
-                for ix, x in enumerate(xs):
-                    for iz, z in enumerate(zs):
-                        process_cell(x, y, z, ix, iy, iz)
-            else:
-                for iz, z in enumerate(zs):
-                    for ix, x in enumerate(xs):
-                        process_cell(x, y, z, ix, iy, iz)
-    elif first_axes_processed == 'z':
-        for iz, z in enumerate(zs):
-            if second_axes_processed == 'x':
-                for ix, x in enumerate(xs):
-                    for iy, y in enumerate(ys):
-                        process_cell(x, y, z, ix, iy, iz)
-            else:
-                for iy, y in enumerate(ys):
-                    for ix, x in enumerate(xs):
-                        process_cell(x, y, z, ix, iy, iz)
-
-    if not processed_result:
-        # Should never happen, I've only seen it on one of four open tabs and it needed to refresh.
-        print("Unexpected error: Processing could not begin, you may need to refresh the tab or restart the service.")
-        return Processed(p, [])
-    elif not any(processed_result.images):
-        print("Unexpected error: draw_xyz_grid failed to return even a single processed image")
-        return Processed(p, [])
-
-    z_count = len(zs)
-
-    for i in range(z_count):
-        start_index = (i * len(xs) * len(ys)) + i
-        end_index = start_index + len(xs) * len(ys)
-        grid = images.image_grid(processed_result.images[start_index:end_index], rows=len(ys))
-        if draw_legend:
-            grid = images.draw_grid_annotations(grid, processed_result.images[start_index].size[0], processed_result.images[start_index].size[1], hor_texts, ver_texts, margin_size)
-        processed_result.images.insert(i, grid)
-        processed_result.all_prompts.insert(i, processed_result.all_prompts[start_index])
-        processed_result.all_seeds.insert(i, processed_result.all_seeds[start_index])
-        processed_result.infotexts.insert(i, processed_result.infotexts[start_index])
-
-    sub_grid_size = processed_result.images[0].size
-    z_grid = images.image_grid(processed_result.images[:z_count], rows=1)
-    if draw_legend:
-        z_grid = images.draw_grid_annotations(z_grid, sub_grid_size[0], sub_grid_size[1], title_texts, [[images.GridAnnotation()]])
-    processed_result.images.insert(0, z_grid)
-    # TODO: Deeper aspects of the program rely on grid info being misaligned between metadata arrays, which is not ideal.
-    # processed_result.all_prompts.insert(0, processed_result.all_prompts[0])
-    # processed_result.all_seeds.insert(0, processed_result.all_seeds[0])
-    processed_result.infotexts.insert(0, processed_result.infotexts[0])
-
-    return processed_result
-
-
 class SharedSettingsStackHelper(object):
     def __enter__(self):
         self.CLIP_stop_at_last_layers = opts.CLIP_stop_at_last_layers
@@ -392,16 +247,13 @@ class SharedSettingsStackHelper(object):
         opts.data["uni_pc_order"] = self.uni_pc_order
         modules.sd_models.reload_model_weights()
         modules.sd_vae.reload_vae_weights()
-
         opts.data["CLIP_stop_at_last_layers"] = self.CLIP_stop_at_last_layers
-
 
 re_range = re.compile(r"\s*([+-]?\s*\d+)\s*-\s*([+-]?\s*\d+)(?:\s*\(([+-]\d+)\s*\))?\s*")
 re_range_float = re.compile(r"\s*([+-]?\s*\d+(?:.\d*)?)\s*-\s*([+-]?\s*\d+(?:.\d*)?)(?:\s*\(([+-]\d+(?:.\d*)?)\s*\))?\s*")
 
 re_range_count = re.compile(r"\s*([+-]?\s*\d+)\s*-\s*([+-]?\s*\d+)(?:\s*\[(\d+)\s*])?\s*")
 re_range_count_float = re.compile(r"\s*([+-]?\s*\d+(?:.\d*)?)\s*-\s*([+-]?\s*\d+(?:.\d*)?)(?:\s*\[(\d+(?:.\d*)?)\s*])?\s*")
-
 
 class Script(scripts.Script):
     def title(self):
@@ -432,6 +284,16 @@ class Script(scripts.Script):
                     a_values = gr.Textbox(label="A values", lines=1, elem_id=self.elem_id("a_values"))
                     fill_a_button = ToolButton(value=fill_values_symbol, elem_id="xyz_grid_fill_a_tool_button", visible=False)
 
+                with gr.Row():
+                    b_type = gr.Dropdown(label="B type", choices=[x.label for x in self.current_axis_options], value=self.current_axis_options[0].label, type="index", elem_id=self.elem_id("b_type"))
+                    b_values = gr.Textbox(label="B values", lines=1, elem_id=self.elem_id("b_values"))
+                    fill_b_button = ToolButton(value=fill_values_symbol, elem_id="xyz_grid_fill_b_tool_button", visible=False)
+
+                with gr.Row():
+                    c_type = gr.Dropdown(label="C type", choices=[x.label for x in self.current_axis_options], value=self.current_axis_options[0].label, type="index", elem_id=self.elem_id("c_type"))
+                    c_values = gr.Textbox(label="C values", lines=1, elem_id=self.elem_id("c_values"))
+                    fill_c_button = ToolButton(value=fill_values_symbol, elem_id="xyz_grid_fill_c_tool_button", visible=False)
+
         with gr.Row(variant="compact", elem_id="axis_options"):
             no_fixed_seeds = gr.Checkbox(label='Keep -1 for seeds', value=False, elem_id=self.elem_id("no_fixed_seeds"))
 
@@ -445,6 +307,8 @@ class Script(scripts.Script):
         fill_y_button.click(fn=fill, inputs=[y_type], outputs=[y_values])
         fill_z_button.click(fn=fill, inputs=[z_type], outputs=[z_values])
         fill_a_button.click(fn=fill, inputs=[a_type], outputs=[a_values])
+        fill_b_button.click(fn=fill, inputs=[b_type], outputs=[b_values])
+        fill_c_button.click(fn=fill, inputs=[c_type], outputs=[c_values])
 
         def select_axis(axis_type, axis_values):
             choices = self.current_axis_options[axis_type].choices
@@ -455,6 +319,8 @@ class Script(scripts.Script):
         y_type.change(fn=select_axis, inputs=[y_type, y_values], outputs=[fill_y_button, y_values])
         z_type.change(fn=select_axis, inputs=[z_type, z_values], outputs=[fill_z_button, z_values])
         a_type.change(fn=select_axis, inputs=[a_type, a_values], outputs=[fill_a_button, a_values])
+        b_type.change(fn=select_axis, inputs=[b_type, b_values], outputs=[fill_b_button, b_values])
+        c_type.change(fn=select_axis, inputs=[c_type, c_values], outputs=[fill_c_button, c_values])
 
         self.infotext_fields = (
             (x_type, "X Type"),
@@ -465,27 +331,28 @@ class Script(scripts.Script):
             (z_values, "Z Values"),
             (a_type, "A Type"),
             (a_values, "A Values"),
+            (b_type, "B Type"),
+            (b_values, "B Values"),
+            (c_type, "C Type"),
+            (c_values, "C Values")
         )
 
         # it's a crime they don't let me pack these into some kind of list
-        return [x_type, x_values, y_type, y_values, z_type, z_values, a_type, a_values, no_fixed_seeds]
+        return [x_type, x_values, y_type, y_values, z_type, z_values, a_type, a_values, b_type, b_values, c_type, c_values, no_fixed_seeds]
 
-    def run(self, p, x_t, x_v, y_t, y_v, z_t, z_v, a_t, a_v, no_fixed_seeds):
-        axis_setup = [(x_t, x_v), (y_t, y_v), (z_t, z_v), (a_t, a_v)]
+    def run(self, p, x_t, x_v, y_t, y_v, z_t, z_v, a_t, a_v, b_t, b_v, c_t, c_v, no_fixed_seeds):
+        axis_setup = [(x_t, x_v), (y_t, y_v), (z_t, z_v), (a_t, a_v), (b_t, b_v), (c_t, c_v)]
         if not no_fixed_seeds:
             modules.processing.fix_seed(p)
-
         p.batch_size = 1
 
         def process_axis(opt, vals):
             if opt.label == 'Nothing':
                 return [0]
-
             if opt.prepare is not None:
                 valslist = opt.prepare(vals)
             else:
                 valslist = csv_string_to_list_strip(vals)
-
             if opt.type == int:
                 valslist_ext = []
                 for val in valslist:
@@ -524,17 +391,14 @@ class Script(scripts.Script):
                 valslist = valslist_ext
             elif opt.type == str_permutations:
                 valslist = list(permutations(valslist))
-
             valslist = [opt.type(x) for x in valslist]
-
             # Confirm options are valid before starting
             if opt.confirm:
                 opt.confirm(p, valslist)
-
             return valslist
 
-        # axis_setup is a list of tuples of axis type names and user inputs
-        # we will retrieve the true axis types using the names
+        # axis_setup is a list of tuples of axis type numbers and user inputs
+        # we will retrieve the true axis types using the numbers
 
         axis_setup = [(self.current_axis_options[atn], atn, av) for atn, av in axis_setup]
 
@@ -566,8 +430,11 @@ class Script(scripts.Script):
                 return sum(av)
             return len(av)
 
-        expected_steps = reduce(lambda x,y: x*y, [expectedStepsForAxis(at, atn, av) for at, atn, av in axis_setup])
         expected_images = reduce(lambda x,y: x*y, [len(av) for at, atn, av in axis_setup]) 
+
+        expected_steps = reduce(lambda x,y: x*y, [expectedStepsForAxis(at, atn, av) for at, atn, av in axis_setup])
+        if 'Steps' not in axis_labels:
+            expected_steps *= p.steps
         if isinstance(p, StableDiffusionProcessingTxt2Img) and p.enable_hr and 'Hires steps' not in axis_labels:
             if p.hr_second_pass_steps:
                expected_steps *= p.hr_second_pass_steps
@@ -581,15 +448,18 @@ class Script(scripts.Script):
 
         state.plot_state = [AxisInfo(at, av) for at, atn, av in axis_setup]
 
-        def cell(axis_setup, axis_values):
+        def draw_single(axis_setup, axis_values):
             if shared.state.interrupted:
                 return Processed(p, [], p.seed, "")
             pc = copy(p)
             pc.styles = pc.styles[:]
+            print()
+            print('setting x/y/z/a axis values:')
             for i in range(len(axis_values)):
                 axis_type = axis_setup[i][0]
                 axis_value = axis_values[i]
                 axis_all_values = axis_setup[i][2]
+                print(f' ** {axis_type.label}: {axis_value}')
                 axis_type.apply(pc, axis_value, axis_all_values)
             try:
                 res = process_images(pc)
@@ -600,9 +470,8 @@ class Script(scripts.Script):
 
         with SharedSettingsStackHelper():
             results_container = None
-            result_index = 1
             for value_combination in product(*[av for at, atn, av in axis_setup]):
-                processed: Processed = cell(axis_setup, value_combination)
+                processed: Processed = draw_single(axis_setup, value_combination)
                 if results_container is None:
                     results_container = copy(processed)
                     results_container.images = []
